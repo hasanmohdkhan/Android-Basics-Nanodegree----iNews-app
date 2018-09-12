f389a954c423f540c2d2d1bc3ba5c2c41304e6cc
hasanmohdkhan <hasanzian@gmail.com>
2018-09-12 15:09:48 +0530

    Added  - Settings Activity

---
+++ app/src/main/java/com/example/hasanzian/newsapp/activity/SettingsActivity.java
@@ -0,0 +1,48 @@
+package com.example.hasanzian.newsapp.activity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.hasanzian.newsapp.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+    public static class NewsPreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+
+            Preference pageSize = findPreference(getString(R.string.settings_min_page_key));
+            bindPreferenceSummaryToValue(pageSize);
+
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
+
+        }
+
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+            preference.setSummary(stringValue);
+            return true;
+        }
+    }
+
+}
